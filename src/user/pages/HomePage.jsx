17f4fb95826c9e4fc1be7e0e696de23ec8059d6b
Joff-delapena <joffersondelapena11@gmail.com>
2026-03-06 19:09:45 +0800

    UPDATED

--- src/user/pages/HomePage.jsx
+++ src/user/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import { getWeeklyReports, getOfficers } from '../../utils/storage';
 import { useEffect, useState, useRef, useCallback } from 'react';
 import { createPortal } from 'react-dom';
