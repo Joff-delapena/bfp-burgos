4f58616658ab9dc3d36f6c1dad0dd0dfaf943e66
Joff-delapena <joffersondelapena11@gmail.com>
2026-03-06 19:45:24 +0800

    remove node_modules from repo

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,73 +1,73 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Toaster } from 'sonner';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { Toaster } from 'sonner';
 
-// User Pages
-import { HomePage } from '../user/pages/HomePage';
-import { AboutPage } from '../user/pages/AboutPage';
-import { OfficersPage } from '../user/pages/OfficersPage';
-import { ContactPage } from '../user/pages/ContactPage';
+// // User Pages
+// import { HomePage } from '../user/pages/HomePage';
+// import { AboutPage } from '../user/pages/AboutPage';
+// import { OfficersPage } from '../user/pages/OfficersPage';
+// import { ContactPage } from '../user/pages/ContactPage';
 
-// Admin Pages
-import { AdminLogin } from '../admin/pages/AdminLogin';
-import { AdminDashboard } from '../admin/pages/AdminDashboard';
+// // Admin Pages
+// import { AdminLogin } from '../admin/pages/AdminLogin';
+// import { AdminDashboard } from '../admin/pages/AdminDashboard';
 
-// User Components
-import { Header } from '../user/components/Header';
-import { Footer } from '../user/components/Footer';
+// // User Components
+// import { Header } from '../user/components/Header';
+// import { Footer } from '../user/components/Footer';
 
-function UserLayout({ children }) {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1">{children}</main>
-      <Footer />
-    </div>
-  );
-}
+// function UserLayout({ children }) {
+//   return (
+//     <div className="min-h-screen flex flex-col">
+//       <Header />
+//       <main className="flex-1">{children}</main>
+//       <Footer />
+//     </div>
+//   );
+// }
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Toaster position="top-right" richColors />
-      <Routes>
-        {/* User/Public Routes */}
-        <Route
-          path="/"
-          element={
-            <UserLayout>
-              <HomePage />
-            </UserLayout>
-          }
-        />
-        <Route
-          path="/about"
-          element={
-            <UserLayout>
-              <AboutPage />
-            </UserLayout>
-          }
-        />
-        <Route
-          path="/officers"
-          element={
-            <UserLayout>
-              <OfficersPage />
-            </UserLayout>
-          }
-        />
-        <Route
-          path="/contact"
-          element={
-            <UserLayout>
-              <ContactPage />
-            </UserLayout>
-          }
-        />
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Toaster position="top-right" richColors />
+//       <Routes>
+//         {/* User/Public Routes */}
+//         <Route
+//           path="/"
+//           element={
+//             <UserLayout>
+//               <HomePage />
+//             </UserLayout>
+//           }
+//         />
+//         <Route
+//           path="/about"
+//           element={
+//             <UserLayout>
+//               <AboutPage />
+//             </UserLayout>
+//           }
+//         />
+//         <Route
+//           path="/officers"
+//           element={
+//             <UserLayout>
+//               <OfficersPage />
+//             </UserLayout>
+//           }
+//         />
+//         <Route
+//           path="/contact"
+//           element={
+//             <UserLayout>
+//               <ContactPage />
+//             </UserLayout>
+//           }
+//         />
 
-        {/* Admin Routes */}
-        <Route path="/admin" element={<AdminLogin />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+//         {/* Admin Routes */}
+//         <Route path="/admin" element={<AdminLogin />} />
+//         <Route path="/admin/dashboard" element={<AdminDashboard />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
